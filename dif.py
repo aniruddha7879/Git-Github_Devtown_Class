9d41e3de13a1cc3049764d2be5f0e9f43f277832
Aniruddha Katkar <aniruddhakatkar5@gmail.com>
2023-06-29 20:07:40 +0530

    Completing dif.py

---
+++ dif.py
@@ -0,0 +1,11 @@
+marks = int(input("Enter your marks : "))
+print(marks)
+print(type(marks))
+
+if (marks >90 ):
+    print("You got First Class!")
+elif(60<=marks>=90):
+    print("You got Second Class!")
+else :
+    print("You got Third Class!")
+
