7cc0430af231340cb738055514eeb81df93602b9
Aniruddha Katkar <aniruddhakatkar5@gmail.com>
2023-06-29 20:06:24 +0530

    MOdified Index.py

---
+++ index.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+
+print ("The addition of two numbers is : ", a + b)
+print ("The substraction of two numbers is : ", a - b)
+print ("The multiplication of two numbers is : ", a * b)
+print ("The division of two numbers is : ", a / b)
